ef8f0a48fcef9cd40ac694d7d2cf8470ad1a1182
Ghulam Mustafa <gm070797@gmail.com>
2023-06-14 22:21:01 +0200

    Update test.sh for 404-Fix

--- project/test.sh
+++ project/test.sh
@@ -6,24 +6,18 @@ project_dir=$(pwd)
 
 echo "=== Executing pipeline ==="
 
-# Change directory to the data folder
-cd ./data
 
 # Execute your pipeline
-python pipeline.py
-
+python project/data/pipline.py
 
 # Validate the output file(s)
-if [ -f "./data/nuremberg_stops_immoscout.sqlite" ]; then
+if [ -f "project/data/nuremberg_stops_immoscout.sqlite" ]; then
   echo "Output file(s) exist."
 else
   echo "Output file(s) not found."
 fi
 
-# Change directory back to the project folder
-cd "$project_dir"
-
 echo "=== Running Tests ==="
-python test.py
+python project/test.py
 
 read -p "Press any key to continue... " -n1 -s
