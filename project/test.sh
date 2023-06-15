d8d8882112dbecc03b6e98fc598a0216779c3bdd
gmMustafa <gm070797@gmail.com>
2023-06-15 18:25:02 +0200

    updated-db-paths

--- project/test.sh
+++ project/test.sh
@@ -8,16 +8,16 @@ echo "=== Executing pipeline ==="
 
 
 # Execute your pipeline
-python "$project_dir/data/pipeline.py"
+python "$project_dir/project/data/pipeline.py"
 
 # Validate the output file(s)
-if [ -f "./project/data/nuremberg_stops_immoscout.sqlite" ]; then
+if [ -f "$project_dir/project/data/nuremberg_stops_immoscout.sqlite" ]; then
   echo "Output file(s) exist."
 else
   echo "Output file(s) not found."
 fi
 
 echo "=== Running Tests ==="
-python "$project_dir/test.py"
+python "$project_dir/project/test.py"
 
 read -p "Press any key to continue... " -n1 -s
